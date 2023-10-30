a0bfca50b5b50b1c481d81e5c1f53eb4daa51e33
Spiros <spiroszoupas@hotmail.com>
2023-10-30 11:39:23 +0000

    add StreamReader that reads the data file.

--- SynecticsAssignment/Program.cs
+++ SynecticsAssignment/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String line;
+
+try
+{
+    StreamReader sr = new StreamReader("C:\\Users\\spiro\\source\\repos\\SynecticsAssignment\\SynecticsAssignment\\DEV-data.txt");
+    line = sr.ReadLine();
+    while (line != null)
+    {
+        line = sr.ReadLine();
+    }
+
+    sr.Close();
+    Console.ReadLine();
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
